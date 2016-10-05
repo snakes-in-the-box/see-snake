1df4e7373886e85925562af080f7f918d1a7f94e
Brent Lippert <brentcl@uga.edu>
2016-10-04 21:16:29 -0400

    Lowered the momentum to .75, made the score change over training closer to what we desire

--- src/main/scala/SeeSnake/Driver.scala
+++ src/main/scala/SeeSnake/Driver.scala
@@ -41,7 +41,7 @@ object Driver {
       .learningRate(learnRate)
       .weightInit(WeightInit.XAVIER)
       .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-      .updater(Updater.NESTEROVS).momentum(0.9)
+      .updater(Updater.NESTEROVS).momentum(0.75)
       .list()
       .layer(0, new ConvolutionLayer.Builder(5, 5)
         //nIn and nOut specify depth. nIn here is the nChannels and nOut is the # of filters to be applied
